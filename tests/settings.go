0e73876cab9945848574bbb0cf348061d4a172ce
Aleksey Chernyshov <molkonor@yandex.ru>
2024-10-04 23:33:05 +0300

    Frontend and tests

---
+++ tests/settings.go
@@ -0,0 +1,7 @@
+package tests
+
+var Port = 7540
+var DBFile = "../scheduler.db"
+var FullNextDate = true
+var Search = true
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzd29yZEhhc2giOiI5ODM0ODc2ZGNmYjA1Y2IxNjdhNWMyNDk1M2ViYTU4YzRhYzg5YjFhZGY1N2YyOGYyZjlkMDlhZjEwN2VlOGYwIn0.uXgW9qIiy4QLRDZpj2WreJOfJopzGjkvQe-1F5tRL-Q`
